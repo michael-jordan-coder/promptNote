ab4c67d5fb9b2ee8df4a53b33ed7f707e53c9f64
npmDada <daniel.gourarye@gmail.com>
2026-02-08 20:19:57 +0200

    Initial Commit

---
+++ PromtSaver/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PromtSaver
+//
+//  Created by gur arye on 2/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PromtSaver/PromtSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromtSaverApp.swift
+//  PromtSaver
+//
+//  Created by gur arye on 2/8/26.
+//
+
+import SwiftUI
+
+@main
+struct PromtSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
